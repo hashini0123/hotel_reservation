a119822784e33558b957063034efa953307e6256
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-04 00:33:33 +0530

    create to customer info add commit

--- src/main/java/controller/CustomerInfoFormController.java
+++ src/main/java/controller/CustomerInfoFormController.java
@@ -1,11 +1,48 @@
 package controller;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import model.dto.CustomerInfoDTO;
+
+import java.sql.*;
 
 public class CustomerInfoFormController {
+
+    public ObservableList<CustomerInfoDTO> getAllCustomers() throws SQLException {
+
+        ObservableList<CustomerInfoDTO> customerInfoDTOS = FXCollections.observableArrayList();
+
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/hotel_reservation_system","root","1234");
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM customers");
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()){
+                customerInfoDTOS.add(new CustomerInfoDTO(
+                        resultSet.getString("customerId"),
+                        resultSet.getString("firstName"),
+                        resultSet.getString("lastName"),
+                        resultSet.getString("email"),
+                        resultSet.getString("city"),
+                        resultSet.getString("registeredDate"),
+                        resultSet.getString("phone"),
+                        resultSet.getString("addresss")
+                ));
+
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return customerInfoDTOS;
+
+    }
+
+
 
     @FXML
     private TableColumn<?, ?> colAddress;
@@ -32,10 +69,12 @@ public class CustomerInfoFormController {
     private TableColumn<?, ?> colRegisteredDate;
 
     @FXML
-    private TableView<?> tblCustomerInfomation;
+    private TableView<CustomerInfoDTO> tblCustomerInfomation;
 
     @FXML
     void btnAddOnAction(ActionEvent event) {
+
+
 
     }
 
