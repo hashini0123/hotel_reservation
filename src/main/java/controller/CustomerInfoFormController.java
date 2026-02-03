ae3689d84f16276b7b7b4100a991b5393e2eb721
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-03 16:23:58 +0530

    create to customer info add commit

--- src/main/java/controller/CustomerInfoFormController.java
+++ src/main/java/controller/CustomerInfoFormController.java
@@ -1,4 +1,57 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class CustomerInfoFormController {
+
+    @FXML
+    private TableColumn<?, ?> colAddress;
+
+    @FXML
+    private TableColumn<?, ?> colCity;
+
+    @FXML
+    private TableColumn<?, ?> colCustomerId;
+
+    @FXML
+    private TableColumn<?, ?> colEmail;
+
+    @FXML
+    private TableColumn<?, ?> colFirstName;
+
+    @FXML
+    private TableColumn<?, ?> colLastName;
+
+    @FXML
+    private TableColumn<?, ?> colPhone;
+
+    @FXML
+    private TableColumn<?, ?> colRegisteredDate;
+
+    @FXML
+    private TableView<?> tblCustomerInfomation;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnReloadOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateOnAction(ActionEvent event) {
+
+    }
+
 }
