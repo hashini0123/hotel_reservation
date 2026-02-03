37df90e8c387a383a6cff8722ae669544465fd64
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-03 16:59:12 +0530

    create to customer info add commit

---
+++ src/main/java/model/dto/CustomerInfoDTO.java
@@ -0,0 +1,30 @@
+package model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class CustomerInfoDTO {
+
+    private String customerId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String city;
+
+    private String registeredDate;
+
+    private String phone;
+
+    private String addresss;
+
+}
